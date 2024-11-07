e32770593395c37decf3b0998a03017e1d3d33cd
Tomer Banay <tomerbanay@Tomers-MacBook-Air.local>
2024-11-07 14:25:13 +0200

    completed

--- FVCalc.java
+++ FVCalc.java
@@ -1,13 +1,15 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
-		Double pv = Double.parseDouble (args[0]);
-		Double r = Double.parseDouble (args[1]);
+		// classify the args
+		double pv = Double.parseDouble (args[0]);
+		double r = Double.parseDouble (args[1]);
 		int t = Integer.parseInt (args[2]);
-		 double fv = (pv * Math.pow (1 + r/100, t));
+
+		// equation of Future Value
+		double fv = (pv * Math.pow ((1 + r/100), t));
 
 		System.out.print("After " + args [2] + " years, $" + args [0] + " saved at " 
-		+ args [1] + "% will yield $" + ((int) fv));
+		+ r + "% will yield $" + ((int) fv));
 	}
 }
