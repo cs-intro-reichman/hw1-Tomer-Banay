0f9ccea58cbbd95177a4707bc9d3e46037cbafbf
Tomer Banay <tomerbanay@Tomers-MacBook-Air.local>
2024-11-07 14:07:38 +0200

    completed

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		Double pv = Double.parseDouble (args[0]);
+		Double r = Double.parseDouble (args[1]);
+		int t = Integer.parseInt (args[2]);
+		 double fv = (pv * Math.pow (1 + r/100, t));
+
+		System.out.print("After " + args [2] + " years, $" + args [0] + " saved at " 
+		+ args [1] + "% will yield $" + ((int) fv));
 	}
 }
