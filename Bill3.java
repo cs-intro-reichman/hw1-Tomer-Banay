3b93b2b4576d5e03ec64e23bfbc7f7ab6e56038a
Tomer Banay <tomerbanay@Tomers-MacBook-Air.local>
2024-11-07 12:20:48 +0200

    completed

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,15 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name2 = args[1];
+		String name3 = args[2];
+		double sub = Double.parseDouble(args [3]);
+
+		//now we will do the math equastion
+		double total = (int) ((sub/3)+1);
+
+		//put the required printed lines
+		System.out.println("Dear " + args[0] +", "+ args[1] +", and "+  args[2] + ": pay " +
+		total + " Shekels each.");
 	}
 }
